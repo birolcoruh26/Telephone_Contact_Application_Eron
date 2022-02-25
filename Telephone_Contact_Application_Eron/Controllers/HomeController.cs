f5e637eb111e3965425ae40dc0e682fb29f2f12c
Birol Coruh <65693604+birolcoruh26@users.noreply.github.com>
2022-02-25 19:18:20 +0300

    update

--- Telephone_Contact_Application_Eron/Controllers/HomeController.cs
+++ Telephone_Contact_Application_Eron/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,7 +13,7 @@ using System.Web;
 using System.Web.Mvc;
 using Telephone_Contact_Application_Eron.Models;
 
-namespace ApiClient.Controllers
+namespace Telephone_Contact_Application_Eron.Controllers
 {
     public class HomeController : Controller
     {
@@ -41,9 +42,10 @@ namespace ApiClient.Controllers
 
         public static string UToken;
 
-            
+
         public dynamic APILogin()
         {
+
             var url = "http://eronsoftware.com:55301/KULLANICI/authentication/";
 
             var httpRequest = (HttpWebRequest)WebRequest.Create(url);
@@ -53,7 +55,7 @@ namespace ApiClient.Controllers
             httpRequest.Headers["ptoken"] = "OPp60lBs9vqqNiAvzM2QPsgVuzHvld4ZShVGqlYqEcEgi2BGFt";
             httpRequest.ContentType = "text";
 
-            var data = @"{""e_kullanici_adi"":""networkAkademi029"",""e_sifre"":""sifre029""}";
+            var data = @"{""e_kullanici_adi"":""networkAkademi028"",""e_sifre"":""sifre028""}";
 
             using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
             {
@@ -69,10 +71,11 @@ namespace ApiClient.Controllers
             result = result.Trim(']');
             dynamic user = JsonConvert.DeserializeObject(result);
             return user;
-        }
-        public ActionResult ApiList()
+        } 
+        static List<Kategori> kategoris = new List<Kategori>();
+        public ActionResult CategoryList()
         {
-            List<Kategori> list = new List<Kategori>();
+            kategoris.Clear();  
             var url = "http://eronsoftware.com:55301/KULLANICI/kategori/";
 
             var httpRequest = (HttpWebRequest)WebRequest.Create(url);
@@ -83,12 +86,51 @@ namespace ApiClient.Controllers
             httpRequest.Headers["utoken"] = UToken;
             httpRequest.ContentType = "text";
 
-            var data = "";
+            var result = "";
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                result = streamReader.ReadToEnd();
+            }
+            dynamic user = JsonConvert.DeserializeObject<List<dynamic>>(result);
 
+            foreach (var item in user)
+            {
+                Kategori a = new Kategori();
+                a.ID = item["e_id"] != null ? Convert.ToInt32(item["e_id"]) : 0;
+                a.KategoriAdi = item["e_kategori_adi"];
+                kategoris.Add(a);
+            }
+            return View(kategoris);
+        }
+
+        public ActionResult CategoryDelete(int id)
+        {
+            Kategori kategori = kategoris.Find(c => c.ID == id);
+            return View(kategori);
+        }
+
+        [HttpPost]
+        public ActionResult CategoryDelete(Kategori kategori)
+        {
+           
+            var url = "http://eronsoftware.com:55301/KULLANICI/kategori/";
+
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "POST";
+
+            httpRequest.Headers["islem"] = "KATEGORI_LISTESI_SIL";
+            httpRequest.Headers["ptoken"] = "OPp60lBs9vqqNiAvzM2QPsgVuzHvld4ZShVGqlYqEcEgi2BGFt";
+            httpRequest.Headers["utoken"] = UToken;
+            httpRequest.ContentType = "text";
+
+            var data = @"{""ESKI_ID"":" + $"{kategori.ID}"+"}";
+            
             using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
             {
                 streamWriter.Write(data);
             }
+
             var result = "";
             var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
             using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
@@ -97,18 +139,10 @@ namespace ApiClient.Controllers
             }
             result = result.Trim('[');
             result = result.Trim(']');
-            //dynamic user = JsonConvert.DeserializeObject(result);
-            JObject user = JsonConvert.DeserializeObject(result) as JObject;
-          
+            dynamic user = JsonConvert.DeserializeObject(result);
 
-            foreach (var item in user)
-            {
-                Kategori a = new Kategori();
-                a.ID = Convert.ToInt32(item["e_id"]);
-                a.KategoriAdi = item["e_kategori_adi"].ToString();
-                list.Add(a);
-            }
-            return View(list);
+            var m = user.MESAJ;
+            return Redirect("/Home/Index");
         }
     }
 }
