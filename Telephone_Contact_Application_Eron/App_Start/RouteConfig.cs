e77d854104fb1027623e95c36cf311a43f223eac
Birol Coruh <65693604+birolcoruh26@users.noreply.github.com>
2022-03-01 19:47:24 +0300

    Update RouteConfig.cs
    ada

--- Telephone_Contact_Application_Eron/App_Start/RouteConfig.cs
+++ Telephone_Contact_Application_Eron/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Telephone_Contact_Application_Eron
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
